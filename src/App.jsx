62c68c75e09facdd0ac3e0a2c2689790a1799b69
Saurabh <sp27243@gmail.com>
2025-12-31 21:03:50 +0530

    Mobile responsive done 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import Sidebar from "./components/Sidebar";
-// import Home from "./pages/Home";
-// import Resume from "./pages/Resume";
-// import Experties from "./pages/Experties";
-// import Contact from "./pages/Contact";
-
-// function App() {
-//   return (
-//     <div className="min-h-screen bg-gradient-to-br from-black via-zinc-900 to-black p-6 text-white flex">
-
-//       {/* Sidebar fixed on the left */}
-//       <Sidebar />
-
-//       {/* Main content */}
-//       <div className="flex-1 ml-72"> {/* ml-72 = sidebar width */}
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/resume" element={<Resume />} />
-//           <Route path="/expertise" element={<Experties />} />
-//           <Route path="/contact" element={<Contact />} />
-//           {/* Add more routes here */}
-//         </Routes>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
@@ -56,14 +26,17 @@ function App() {
 
       {/* ================= MOBILE VIEW ================= */}
       <div className="md:hidden p-4">
-        <Sidebar mobile />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/expertise" element={<Experties />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <div className="mobile-side">
+          <Sidebar mobile />
+        </div>
+        <div className="mobile-section">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/expertise" element={<Experties />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
